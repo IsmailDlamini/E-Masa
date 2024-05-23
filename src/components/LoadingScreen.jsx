218e859288f8a949ee1b4cb1e851b64d63d11813
Ismail <iii409475@gmail.com>
2024-05-23 02:04:09 +0200

    Still working on the loading page

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,15 @@
+import "./LoadingScreen.css"
+
+const LoadingScreen = () => {
+  return (
+    <>
+    
+        <div className="loading-screen-container">
+            
+        </div>
+
+    </>
+  )
+}
+
+export default LoadingScreen
